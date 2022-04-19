a02e3118c555281b7c03268bdf0270e19dda178d
Gonzalo Tórtola <gontortola@gmail.com>
2022-04-19 00:27:22 -0300

    Desafío Entregable 1 (versión final)

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
 let continuar = true
 
+const recargo5 = 1.05
+const recargo10 = 1.10
+const recargo15 = 1.15
+
 while(continuar) {
-    
-    alert(".:: BIENVENIDO A E-COMMERCE ::.\n\nIngrese el nombre del producto para ver su precio:\n1. Remera\n2. Pantalón\n3. Musculosa\n4. Short\n\nPara salir ingrese: ESC");
 
-    producto = prompt("Ingrese el nombre del producto:").toLowerCase();
+    alert('.:: CALCULADOR DE CUOTAS ::.');
 
-    if(producto === "remera") {
+    montoAPagar = parseFloat(prompt('Por favor, ingrese el monto a pagar:'));
+    cantCuotas = parseInt(prompt('Por favor, ingrese la cantidad de cuotas:'));
 
-        alert("El precio de la remera es de $2.199,99 de contado.");
-
-    } else if(producto === "pantalon" || producto === "pantalón") {
-
-        alert("El precio del pantalón es de $1.699,99 de contado.");
-
-    } else if(producto === "musculosa"){
-
-        alert("El precio de la musculosa es de $1.899,99 de contado.");
-
-    } else if(producto === "short"){
-
-        alert("El precio del short es de $1.299,99 de contado.");
-
-    } else if(producto === "esc"){
-
-        alert("Gracias por su visita.");
-        continuar = false;
-
+    if(!(isNaN(montoAPagar) || isNaN(cantCuotas))) {
+        continuar = false
     } else {
-
-        alert("Por favor, ingrese un nombre de producto válido.");
+        alert('Por favor, ingrese un valor numérico válido.')
     }
+
+    function pagoEnCuotas (monto, cuotas) {
+
+        if(cuotas > 0 && cuotas <= 3) {
+            pagoTotal = monto/cuotas;
+            alert('Pago sin interés: usted abonará $'+pagoTotal.toFixed(2)+' cada mes, durante '+cuotas+' meses.')
+
+        } else if(cuotas > 3 && cuotas <= 6) {
+            pagoTotal = (monto/cuotas)*recargo5;
+            alert('5% de recargo: usted abonará $'+pagoTotal.toFixed(2)+' cada mes, durante '+cuotas+' meses.')
+
+        } else if(cuotas > 6 && cuotas <= 9) {
+            pagoTotal = (monto/cuotas)*recargo10;
+            alert('10% de recargo: usted abonará $'+pagoTotal.toFixed(2)+' cada mes, durante '+cuotas+' meses.')
+
+        } else if(cuotas > 9 && cuotas <= 12) {
+            pagoTotal = (monto/cuotas)*recargo15;
+            alert('15% de recargo: usted abonará $'+pagoTotal.toFixed(2)+' cada mes, durante '+cuotas+' meses.')
+
+        } else {
+            alert('Ingrese un número de cuotas válido. Recuerde que puede ser de 1 a 12.')
+        }
+    }
+
+    pagoEnCuotas (montoAPagar, cantCuotas);
 }
