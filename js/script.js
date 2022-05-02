3301cb4cdb2467bdea2f3f83c992db3a2e3b2f11
Gonzalo Tórtola <gontortola@gmail.com>
2022-05-01 23:53:49 -0300

    PreEntrega 1 (v1)

--- js/script.js
+++ js/script.js
@@ -1,63 +1,126 @@
-// Constructor de producto
-class Producto {    
+class Producto {
     constructor(nombre, categoria, precio, codigo, stock) {
-        this.nombre = nombre.toUpperCase();
-        this.categoria = categoria.toUpperCase();
-        this.precio = parseFloat(precio).toFixed(2);
+        this.nombre = nombre;
+        this.categoria = categoria;
+        if (this.categoria == 1) {
+            this.categoria = "Urban";
+        } else if(this.categoria == 2) {
+            this.categoria = "Energy Life";
+        } else if(this.categoria == 3) {
+            this.categoria = "Cosmetica";
+        } else if(this.categoria == 4) {
+            this.categoria = "Perfumeria";
+        }
+        this.precio = parseFloat(precio);
         this.codigo = codigo.toUpperCase();
         this.stock = parseInt(stock);
+        this.disponibilidad = true;
+    }
+
+    sumarIVA() {
+        return this.precio * 1.21;
+    }
+
+    vender() {
+        this.disponibilidad = false;
+    }
+
+    precioSugerido() {
+        return this.precio * 1.21 * 1.25;
     }
 }
 
-// Declaración de array vacío para los productos que ingresará el usuario
-let productosNuevos = [];
+let producto1 = new Producto("Musculosa Amarilla", 2, "1590", "EL-58", 2);
+let producto2 = new Producto("Pantalón Negro", 1, "2999", "U-05", 0);
+let producto3 = new Producto("Perfume Red Seduction", 4, "1780", "P-01", 20);
 
-// Declaración de productos existentes
-let productoExistente1 = new Producto("Perfume Meira", "Eau de perfum", "1599,99", "ALQ-01", 50);
-let productoExistente2 = new Producto("Remera Maywa", "Energy Life", "1299,99", "EL-02", 28);
+productosExistentes = [producto1, producto2, producto3];
 
-// Declaración de array con productos existentes
-let listaVieja = [productoExistente1, productoExistente1];
+productosIngresados = [];
 
-// Declaración de nuevos productos por el usuario
-// Producto 1
-alert("Ingrese los datos de su nuevo producto #1.")
+continua = true
+while (continua) {
+    let validador = prompt("Ingrese el nombre del producto a añadir o ingrese 'FIN' para finalizar.")
+    if (validador === "FIN" || validador === "fin" || validador === "Fin") {
+        continua = false;
+    } else {
+        nombreProducto = validador;
+        let categoriaProducto = parseInt(prompt("Ingrese el número según la categoría del producto:\n1. Urban\n2. Energy Life\n3. Cosmética\n4. Perfumeria"))
+        let precioProducto = prompt("Ingrese el precio del producto añadido.");
+        let codigoProducto = prompt("Ingrese el código del producto añadido.");
+        let stockProducto = prompt("Ingrese la cantidad de stock del producto añadido.");
+        productosIngresados.push(new Producto(nombreProducto, categoriaProducto, precioProducto, codigoProducto, stockProducto));
+    }
+}
 
-let entradaNombre1 = prompt("Ingrese el nombre del producto #1:");
-let entradaCategoria1 = prompt("Ingrese la categoría del producto #1:");
-let entradaPrecio1 = prompt("Ingrese el precio del producto #1:");
-let entradaCodigo1 = prompt("Ingrese el código del producto #1:");
-let entradaStock1 = prompt("Ingrese el stock del producto #1:");
+nuevaListaDeProductos = productosExistentes.concat(productosIngresados);
 
-let producto1 = new Producto(entradaNombre1, entradaCategoria1, entradaPrecio1, entradaCodigo1, entradaStock1);
+console.log(nuevaListaDeProductos);
 
-productosNuevos.push(producto1);
-console.log(productosNuevos.concat(listaVieja));
+for (let producto of productosIngresados) {
+    alert("PRODUCTO AÑADIDO >");
+    alert("Nombre: "+ producto.nombre +"\nCategoría: "+ producto.categoria +"\nPrecio: $"+ producto.precio +"\nPrecio (con IVA incluido): $"+ (producto.sumarIVA()).toFixed(2) +"\nPrecio sugerido para la venta: $"+ producto.precioSugerido().toFixed(2) +"\nCódigo: "+ producto.codigo +"\nStock: "+ producto.stock +"");
+};
 
-// Producto 2
-alert("Ingrese los datos de su nuevo producto #2.")
+for (let producto of productosExistentes) {
+    alert("PRODUCTO EXISTENTE >");
+    alert("Nombre: "+ producto.nombre +"\nCategoría: "+ producto.categoria +"\nPrecio: $"+ producto.precio +"\nPrecio (con IVA incluido): $"+ (producto.sumarIVA()).toFixed(2) +"\nPrecio sugerido para la venta: $"+ producto.precioSugerido().toFixed(2) +"\nCódigo: "+ producto.codigo +"\nStock: "+ producto.stock +"");
+};
 
-let entradaNombre2 = prompt("Ingrese el nombre del producto #2:");
-let entradaCategoria2 = prompt("Ingrese la categoría del producto #2:");
-let entradaPrecio2 = prompt("Ingrese el precio del producto #2:");
-let entradaCodigo2 = prompt("Ingrese el código del producto #2:");
-let entradaStock2 = prompt("Ingrese el stock del producto #2:");
+// ALERTA POCO STOCK
 
-let producto2 = new Producto(entradaNombre2, entradaCategoria2, entradaPrecio2, entradaCodigo2, entradaStock2);
+let pocoStock = nuevaListaDeProductos.filter(producto => ((producto.stock <= 3) && (producto.stock > 0)));
+console.log("Productos con STOCK ESCASO: ");
+console.log(pocoStock);
 
-productosNuevos.push(producto2);
-console.log(productosNuevos.concat(listaVieja));
+for (let producto of pocoStock) {
+    alert("Productos con stock escaso (menos de 3 unidades):");
+    alert("PRODUCTOS CON STOCK ESCASO:\n\nNombre: "+ producto.nombre +"\nCategoría: "+ producto.categoria +"\nCódigo: "+ producto.codigo +"\nStock: "+ producto.stock +"");
+};
 
-// Producto 3
-alert("Ingrese los datos de su nuevo producto #3.")
+// ALERTA SIN STOCK
 
-let entradaNombre3 = prompt("Ingrese el nombre del producto #3:");
-let entradaCategoria3 = prompt("Ingrese la categoría del producto #3:");
-let entradaPrecio3 = prompt("Ingrese el precio del producto #3:");
-let entradaCodigo3 = prompt("Ingrese el código del producto #3:");
-let entradaStock3 = prompt("Ingrese el stock del producto #3:");
+let sinStock = nuevaListaDeProductos.filter(producto => producto.stock == 0 || producto.disponibilidad == false);
+console.log("Productos sin stock:");
+console.log(sinStock);
 
-let producto3 = new Producto(entradaNombre3, entradaCategoria3, entradaPrecio3, entradaCodigo3, entradaStock3);
+for (let producto of sinStock) {
+    alert("Productos sin stock / sin disponibilidad:");
+    alert("Productos sin stock\n\nNombre: "+ producto.nombre +"\nCategoría: "+ producto.categoria +"\nCódigo: "+ producto.codigo +"");
+};
 
-productosNuevos.push(producto3);
-console.log(productosNuevos.concat(listaVieja));
+// BÚSQUEDA DE PRODUCTOS
+
+let busqueda = prompt("Ingrese el producto que desee buscar:");
+let resultadoBusqueda = nuevaListaDeProductos.filter((producto) => producto.nombre.includes(busqueda));
+console.log("Resultado de búsqueda:")
+console.log(resultadoBusqueda);
+for (let producto of resultadoBusqueda) {
+    alert("Resultado de búsqueda:\n\nNombre: "+ producto.nombre +"\nCategoría: "+ producto.categoria +"\nPrecio: $"+ producto.precio +"\nPrecio (con IVA incluido): $"+ (producto.sumarIVA()).toFixed(2) +"\nPrecio sugerido para la venta: $"+ producto.precioSugerido().toFixed(2) +"\nCódigo: "+ producto.codigo +"\nStock: "+ producto.stock +"");
+};
+
+// ORDENAR POR CANTIDAD STOCK
+// MENOR A MAYOR
+let ordenStockMenorAMayor = nuevaListaDeProductos;
+ordenStockMenorAMayor.sort((a, b) => a.stock - b.stock);
+console.log('Ordenados por stock: de menor a mayor: ');
+console.log(ordenStockMenorAMayor);
+
+// MAYOR A MENOR
+let ordenStockMayorAMenor = nuevaListaDeProductos;
+ordenStockMayorAMenor.sort((a, b) => b.stock - a.stock);
+console.log('Ordenados por stock: de mayor a menor: ');
+console.log(ordenStockMayorAMenor);
+
+// ORDENAR POR PRECIO
+// MENOR A MAYOR
+let ordenPrecioMenorAMayor = nuevaListaDeProductos;
+ordenPrecioMenorAMayor.sort((a, b) => a.precio - b.precio);
+console.log('Ordenados por precio: de menor a mayor: ');
+console.log(ordenPrecioMenorAMayor);
+
+// MAYOR A MENOR
+let ordenPrecioMayorAMenor = nuevaListaDeProductos;
+ordenPrecioMayorAMenor.sort((a, b) => b.precio - a.precio);
+console.log('Ordenados por precio: de mayor a menor: ');
+console.log(ordenPrecioMayorAMenor);
